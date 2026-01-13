d7e92826394e915d46d885af4b6c1905e84ff8c9
Steven <bubuchacha@outlook.com>
2026-01-13 11:40:43 +0800

    ver 0.0.53

--- js/albums.config.js
+++ js/albums.config.js
@@ -1,5 +1,5 @@
 window.ALBUMS = {
-  about:   { label: 'About',       folder: '/VermaVibe_Web_dev/images/About',  manifest: 'manifest.json', count: 12, ext: 'PNG', pad: 0, type: 'images' },
+  about:   { label: 'About', folder: '', manifest: '', count: 0, ext: '', pad: 0, type: 'content' },
   tech:    { label: 'Tech',        folder: '/VermaVibe_Web_dev/images/Tech',   manifest: 'manifest.json', count: 18, ext: 'PNG', pad: 0, type: 'images' },
   cases:   { label: 'Case',        folder: '/VermaVibe_Web_dev/images/Cases',  manifest: 'manifest.json', count: 20, ext: 'PNG', pad: 0, type: 'images' },
   collaborate:  { label: 'Collaborate', folder: '/VermaVibe_Web_dev/images/Collab', manifest: 'manifest.json', count: 10, ext: 'PNG', pad: 0, type: 'images' },
